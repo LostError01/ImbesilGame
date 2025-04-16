b8d622599ddcf0dd474abda4c9de82434573d9ad
LostError01 <ishi@live.com.mx>
2025-04-16 12:28:47 -0600

    Elevaciones

--- Assets/Scripts/Escaleras.cs
+++ Assets/Scripts/Escaleras.cs
@@ -5,6 +5,8 @@ public class Escaleras : MonoBehaviour
     public Collider2D[] collidersMontania;
     public Collider2D[] collidersLimites;
 
+    public int LayerPlayer;
+
     private void OnTriggerEnter2D(Collider2D collision) //Al entrar en trigger, desactiva el collider de la montaña para poder caminar
     {
         if (collision.gameObject.CompareTag("Player"))
@@ -19,7 +21,7 @@ public class Escaleras : MonoBehaviour
                 limite.enabled = true; //Activa los limites de la montaña al entrar
             }
 
-            collision.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 15; // Cambia la capa a 15 (encima de la montaña)
+            collision.gameObject.GetComponent<SpriteRenderer>().sortingOrder = LayerPlayer; // Cambia la capa a 15 (encima de la montaña)
         }
     }
 }
--- Assets/Scripts/EscalerasSalir.cs
+++ Assets/Scripts/EscalerasSalir.cs
@@ -5,6 +5,8 @@ public class EscalerasSalir : MonoBehaviour
     public Collider2D[] collidersMontania;
     public Collider2D[] collidersLimites;
 
+    public int LayerPlayer;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
@@ -19,7 +21,7 @@ public class EscalerasSalir : MonoBehaviour
                 limite.enabled = false; //Desactiva los limites de la montaña al salir
             }
 
-            collision.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 5; // Cambia la capa a 5 de nuevo
+            collision.gameObject.GetComponent<SpriteRenderer>().sortingOrder = LayerPlayer; // Cambia la capa a 5 de nuevo
         }
     }
 }
