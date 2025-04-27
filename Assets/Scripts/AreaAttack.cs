6451d7d61a0fbfd2e9c57aee83c2668566c3f8b0
LostError01 <ishi@live.com.mx>
2025-04-26 23:34:19 -0600

    Roca 2

--- Assets/Scripts/AreaAttack.cs
+++ Assets/Scripts/AreaAttack.cs
@@ -5,13 +5,4 @@ using System.Collections;
 public class AreaAttack : MonoBehaviour
 {
     public SceneTransition transition;
-    void OnTriggerEnter2D(Collider2D col)
-    {
-        // Verificar trigger con el BoxCollider2D 'enemigo'
-        if (col.CompareTag("Enemy"))
-        {
-            //Destruir el objeto del enemigo
-            Destroy(col.gameObject);
-        }
-    }
 }
