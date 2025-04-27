f75e374475c0028e2dd440851c965fef21b48a10
LostError01 <ishi@live.com.mx>
2025-04-26 18:16:02 -0600

    Piedra

--- Assets/Scripts/AreaAttack.cs
+++ Assets/Scripts/AreaAttack.cs
@@ -5,20 +5,13 @@ using System.Collections;
 public class AreaAttack : MonoBehaviour
 {
     public SceneTransition transition;
-    public BoxCollider2D enemigo;
-    public Rigidbody2D rbEnemy;
     void OnTriggerEnter2D(Collider2D col)
     {
         // Verificar trigger con el BoxCollider2D 'enemigo'
-        if (col.Equals(enemigo))
+        if (col.CompareTag("Enemy"))
         {
-            // Desactivar el Rigidbody2D del enemigo
-            rbEnemy.linearVelocity = Vector2.zero;
-
             //Destruir el objeto del enemigo
             Destroy(col.gameObject);
-
-            transition.LoadSceneWithFade("BattleScene01");
         }
     }
 }
